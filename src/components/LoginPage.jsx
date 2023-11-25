7505b24676cb84fca751223a78445c4ba9d36b68
Jordan Femi-Bella <femibellajordan@gmail.com>
2023-11-25 09:27:27 +0100

    login complete

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,62 +1,52 @@
 import React, { useState } from "react";
 import LoadingIcons from "react-loading-icons";
 import axios from "axios";
+import { jwtDecode } from "jwt-decode";
 
 const LoginPage = ({ onLogin }) => {
   const [showOTPInput, setShowOTPInput] = useState(false);
   const [otp, setOTP] = useState(["", "", "", ""]);
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState(""); 
+  const [email, setEmail] = useState(""); 
   const [password, setPassword] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-
-//   const [countdown, setCountdown] = useState(300); // Add this line
-
-//   useEffect(() => {
-//     if (showOTPInput && countdown > 0) {
-//       const timer = setTimeout(() => {
-//         setCountdown(countdown - 1);
-//       }, 1000);
-//       return () => clearTimeout(timer);
-//     }
-//   }, [showOTPInput, countdown]);
+  const [isOTPLoading, setIsOTPLoading] = useState(false);
 
   const handleLoginClick = async () => {
     setIsLoading(true);
+    setShowOTPInput(true); 
+
     try {
-      // Make a request to the login endpoint
       const response = await axios.post(
         "https://3gl1qmkg-4000.uks1.devtunnels.ms/admin_service/login",
         {
-          email,
+          username,
           password,
         }
       );
 
-      // If login is successful, send the OTP
+      console.log("Login response status:", response.status); 
+
       if (response.status === 200) {
         const otpResponse = await axios.post(
           "https://3gl1qmkg-4000.uks1.devtunnels.ms/admin_service/generate_otp_code",
           {
-            email: email,
+            email,
           }
         );
 
-        // If OTP is successfully sent, show the OTP input field
-        if (otpResponse.status === 200) {
-          setShowOTPInput(true);
-        }
+        console.log("OTP response status:", otpResponse.status); 
       }
     } catch (error) {
-      // Handle login error
       console.error("Login failed:", error.message);
-      setErrorMessage("Unauthorized to login");
+      setErrorMessage("failed to login");
     } finally {
       setIsLoading(false);
     }
   };
 
-  const handleOTPChange = async (index, value) => {
+  const handleOTPChange = (index, value) => {
     // Update the OTP array with the entered digit
     const newOTP = [...otp];
     newOTP[index] = value;
@@ -66,23 +56,45 @@ const LoginPage = ({ onLogin }) => {
     if (value && index < otp.length - 1) {
       document.getElementById(`otp-input-${index + 1}`).focus();
     }
+  };
 
-    // If all OTP digits are entered, verify OTP with the backend
-    if (index === otp.length - 1 && !newOTP.includes("")) {
+  const handleOTPSubmit = async () => {
+    setIsOTPLoading(true);
+    if (!otp.includes("")) {
       try {
         // Make a request to verify the OTP
-        const response = await axios.post("backend_verify_otp_endpoint", {
-          email,
-          otp: newOTP.join(""),
-        });
+        const response = await axios.post(
+          "https://3gl1qmkg-4000.uks1.devtunnels.ms/admin_service/verify_otp_code",
+          {
+            email_to_verify: email,
+            otp_to_verify: otp.join(""),
+          }
+        );
 
-        // If OTP verification is successful, call onLogin
+        console.log("OTP verification response status:", response.status); 
+
+        // If OTP verification is successful, decode the token and send it as a header
         if (response.status === 200) {
+          const { token } = response.data;
+          const decodedToken = jwtDecode(token);
+
+          // Send the decoded token as a header
+          const config = {
+            headers: { Authorization: `Bearer ${decodedToken}` },
+          };
+
+          const response = await axios.get(
+            "https://3gl1qmkg-4000.uks1.devtunnels.ms/admin_service/verify_otp_code",
+            config
+          );
+
           onLogin();
         }
       } catch (error) {
         // Handle OTP verification error
         console.error("OTP verification failed:", error.message);
+        setIsOTPLoading(false);
+        setErrorMessage("OTP verification failed");
       }
     }
   };
@@ -95,6 +107,16 @@ const LoginPage = ({ onLogin }) => {
         {showOTPInput ? (
           <div>
             <h3 className="text-lg font-semibold mb-4">Enter OTP</h3>
+            <label className="block text-gray-700 text-sm font-bold mb-2">
+              Email
+            </label>
+            <input
+              className="border outline-none rounded w-full py-2 px-3 mb-3"
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <div className="flex space-x-4">
               {otp.map((digit, index) => (
                 <input
@@ -108,26 +130,29 @@ const LoginPage = ({ onLogin }) => {
                 />
               ))}
             </div>
-            <p className=" my-2">
-                Your OTP is valid for 5 minutes
-              {/* Your OTP is valid for {Math.floor(countdown / 60)} minutes and{" "}
-              {countdown % 60} seconds. */}
-            </p>
-            <button className="bg-[#ef6426] hover:bg-[#cb5925] text-white font-bold py-2 px-4 rounded-full">
-              Verify OTP
+            <p className=" my-2">Your OTP is valid for 3 minutes</p>
+            <button
+              className="bg-[#ef6426] hover:bg-[#cb5925] text-white font-bold py-2 px-4 rounded-full"
+              onClick={handleOTPSubmit} 
+            >
+              {isOTPLoading ? (
+                <LoadingIcons.ThreeDots width="30px" />
+              ) : (
+                "Verify OTP"
+              )}
             </button>
           </div>
         ) : (
           <>
             <label className="block text-gray-700 text-sm font-bold mb-2">
-              Email
+              Username 
             </label>
             <input
               className="border outline-none rounded w-full py-2 px-3 mb-3"
-              type="email"
-              placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              type="text"
+              placeholder="Enter your username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
             <label className="block text-gray-700 text-sm font-bold mb-2">
               Password
@@ -141,12 +166,12 @@ const LoginPage = ({ onLogin }) => {
             />
             <button
               className={`bg-[#ef6426]  text-white font-bold py-2 px-4 rounded-full w-full flex items-center justify-center ${
-                isLoading || !email || !password
+                isLoading || !username || !password
                   ? "bg-gray-400 cursor-not-allowed"
                   : "hover:bg-[#cb5925]"
               }`}
               onClick={handleLoginClick}
-              disabled={isLoading || !email || !password}
+              disabled={isLoading || !username || !password}
             >
               {isLoading ? <LoadingIcons.ThreeDots width="30px" /> : "Login"}
             </button>
